ed49927e71e9af0d700bbe4f864158d48af16a36
enskose <enskose1@gmail.com>
2024-08-22 13:19:46 +0300

    SelectWorks

--- SelectWorks.sql
+++ SelectWorks.sql
@@ -5,4 +5,17 @@ select * from customers where country = 'UK' and city = 'Cowes'
 select * from customers where region is null
 
 -- tarih ile ilgili alanların sorgulanması 
-select * from orders where date_part('year', order_date) = 1996
+select * from orders where date_part('year', order_date) = 1996 and date_part('month', order_date) = 04 and date_part('day', order_date) = 16
+
+-- amerika dışındaki tedarikçilerin listesi
+select * from suppliers where country != 'USA'
+
+-- işlenecek konular
+-- like, sum, count, avg fonksiyonları, sub query 
+-- gruplama ve having 
+-- join (inner, left, right, full)
+-- procedure, view, trigger, konuları ele alınacak
+-- crud işlemleri Create, Update, Delete
+
+-- Kendi databese'imizi oluşturma
+-- Normalization 
