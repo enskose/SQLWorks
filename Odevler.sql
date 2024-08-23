b5583b375d2584c774f76c70f5518b02a00e5c5b
enskose <enskose1@gmail.com>
2024-08-23 13:14:06 +0300

    SQL Ödev 1

---
+++ Odevler.sql
@@ -0,0 +1,16 @@
+-- Ülkelerin yaptığı ciroları bulalım
+
+
+-- Ülkelerin yaptığı ciroları yıllara göre dağıtalım
+
+
+-- Çalışanların yaptığı ciro nedir? (1997 yılı)
+
+
+-- Kargo firmalarının taşıdığı siparişlerin toplam değeri nedir?
+
+
+-- Kategorilere göre ciro nedir?
+
+
+-- 1997 yılındaki ciroların aylara göre dağılımları nedir?
