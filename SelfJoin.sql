561b6e0053cc036275f4bae4d74add6f10bcb015
enskose <enskose1@gmail.com>
2024-08-29 08:40:53 +0300

    Self Join

--- SelfJoin.sql
+++ SelfJoin.sql
@@ -6,7 +6,7 @@ from Employees e1
 inner join Employees e2 on e1.EmployeeID=e2.ReportsTo
 
 -- postgresql 
-select e2.Employee_ID ,e2.First_Name || ' ' || e2.Last_Name,' => ' || e1.First_Name || ' ' || e1.Last_Name , e1.Employee_ID
+select e2.Employee_ID, e2.First_Name || ' ' || e2.Last_Name, ' => ' || e1.First_Name || ' ' || e1.Last_Name, e1.Employee_ID
 from Employees e1
-inner join Employees e2 on e1.Employee_ID=e2.Reports_To
+inner join Employees e2 on e1.Employee_ID = e2.Reports_To
 
